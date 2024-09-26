1e19a19441c98ca39cb1c988daf0547d6314eb4a
Ritesh jha <jhar27018@gmail.com>
2024-09-26 17:39:28 +0530

    department coordinator student page and attendance entry page is done

--- department_coordinator/urls.py
+++ department_coordinator/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("",views.index,name='department_coordinator_index'),
     path("stats/",views.stats,name='department_coordinator_stats'),
-    path("attendance/",views.stats,name='department_coordinator_attendance'),
+    path("attendance/",views.attendance,name='department_coordinator_attendance'),
 ]
