6e854e94ebea23156f6508832887057da3bb0515
Ritesh jha <jhar27018@gmail.com>
2024-10-01 21:05:21 +0530

    data integrated

--- principal/urls.py
+++ principal/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.TnPStats, name="principal_index"),
     path("internship/", views.internship, name="principal_internship"),
+    path("placement/", views.placement, name="principal_placement"),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render, redirect
 import json
 from django.contrib.auth.decorators import login_required
-
+import os
+from django.conf import settings
 
 # Create your views here.
 @login_required
 def TnPStats(request):
     if request.user.role != "principal":
         return redirect("/")
-    return render(request, "principal/index.html")
+    filePath = os.path.join(settings.BASE_DIR, "static/Data/training_data.json")
+    try:
+        with open(filePath, "r") as file:
+            data = json.load(file)
+            Student_consent_for_Trainning_Institute = data.get(
+                "Student_consent_for_Trainning_Institute",
+            )
+            Test_score = data.get("Test_score", {})
+    except Exception as e:
+        print(f"Error reading JSON file: {e}")
+        Student_consent_for_Trainning_Institute = {}
+        Test_score = {}
+
+    context = {
+        "Student_consent_for_Trainning_Institute": json.dumps(
+            Student_consent_for_Trainning_Institute
+        ),
+        "Test_score": json.dumps(Test_score),
+    }
+
+    return render(request, "principal/index.html", context)
+
 
 
 @login_required
 def internship(request):
-    # print(request.user.role)
     if request.user.role != "principal":
         return redirect("/")
     branch_data = {
@@ -60,3 +81,46 @@ def internship(request):
         "internship_bar_labels": json.dumps(internship_bar_labels),
     }
     return render(request, "principal/internship.html", context)
+
+@login_required
+def placement(request):
+    if request.user.role != "principal":
+        return redirect("/")
+    json_file_path = os.path.join(settings.BASE_DIR, "static/Data/alumni_data_2024.json")
+    json_file_path_placement = os.path.join(settings.BASE_DIR, "static/Data/placement_data.json")
+    try:
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+            consent_graph = data.get("Consent_graph")  # Extract only "Consent_graph"
+            consent_counts_by_branch = data.get("consent_counts_by_branch")
+            print(consent_counts_by_branch)
+            top_10_employers = data.get("top_10_employers")
+            placement_distribution_by_branch = data.get(
+                "placement_distribution_by_branch"
+            )
+            average_salary_by_branch = data.get("average_salary_by_branch")
+
+        with open(json_file_path_placement, "r") as file:
+            placement_data = json.load(file)
+            Total_placements_comparison = placement_data.get(
+                "Total_placements_comparison"
+            )
+            branch_comparison = placement_data.get("branch_comparison")
+
+    except Exception as e:
+        print(f"Error reading JSON file: {e}")
+        consent_graph = {}
+
+    context = {
+        "consent_graph": json.dumps(consent_graph),
+        "consent_counts_by_branch": json.dumps(consent_counts_by_branch),
+        "top_10_employers": json.dumps(top_10_employers),
+        "placement_distribution_by_branch": json.dumps(
+            placement_distribution_by_branch
+        ),
+        "average_salary_by_branch": json.dumps(average_salary_by_branch),
+        "Total_placements_comparison": json.dumps(Total_placements_comparison),
+        "branch_comparison": json.dumps(branch_comparison),
+    }  
+
+    return render(request, "principal/placement.html", context)
